e0331d77931dfd21c1948153b397d62dcc89cccb
DanDan-Dan <148914957+DanDan-Dan@users.noreply.github.com>
2025-02-01 12:15:28 -0500

    climb
    - change the elevator
    - add the constant
    -climb

--- src/main/java/frc/robot/subsystems/climbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climbSubsystem.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems;
 
-public class climbSubsystem {
+// double solenoid 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/* Channel: 0,1
+climberUp
+climberDown  */
+public class climbSubsystem extends SubsystemBase {
+
+    private DoubleSolenoid mClimDoubleSolenoid;
+
+    public climbSubsystem() {
+
+        mClimDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    }
+
+    public void climberUp() {
+        mClimDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void climberDown() {
+        mClimDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
 
 }
--- src/main/java/frc/robot/subsystems/elevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevatorSubsystem.java
@@ -1,117 +1,110 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 
-public class elevator extends SubsystemBase{
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorSetPoint;
+
+public class elevatorSubsystem extends SubsystemBase {
 
     private final SparkFlex motor;
     private final SparkClosedLoopController controller;
     private final RelativeEncoder encoder;
     private final SparkFlexConfig config;
-  
+
     private double targetPosition;
 
-    /* goToBottom
-    gotToReefLevel1
-    gotToReefLevel2
-    gotToReefLevel3
-    gotToReefLevel4
-    gotToHPStation
-    goToBarge
-    goToAlgae1-3 */  
-    public elevator(){
-        motor= new SparkFlex(12, MotorType.kBrushless);
+    public elevatorSubsystem() {
+        // configure motor
+        motor = new SparkFlex(12, MotorType.kBrushless);
         controller = motor.getClosedLoopController();
 
-     // Configure encoder
-    encoder = motor.getEncoder();
-    encoder.setPosition(0);
+        // Configure encoder
+        encoder = motor.getEncoder();
+        encoder.setPosition(0);
 
-    // Configure motor properties
-    config = new SparkFlexConfig();
-    config.idleMode(IdleMode.kBrake);
-    config.inverted(false);
+        // Configure motor properties
+        config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kBrake);
+        config.inverted(false);
 
-    // Configure encoder conversion factors
-    config.encoder
-      .positionConversionFactor(0)
-      .velocityConversionFactor(0);
+        // Configure encoder conversion factors
+        config.encoder
+                .positionConversionFactor(0)
+                .velocityConversionFactor(0);
 
-    config.closedLoop.maxMotion
-       .maxVelocity(0)
-       .maxAcceleration(0)
-       .allowedClosedLoopError(2);
+        config.closedLoop.maxMotion
+                .maxVelocity(0)
+                .maxAcceleration(0)
+                .allowedClosedLoopError(2);
 
-    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
 
-    
     @Override
     public void periodic() {
-       
+
     }
 
-    public void goToBottom(){
-        targetPosition = 0;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
-    }
-    
-    public void gotToReefLevel1(){
-        targetPosition = 1;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void goToBottom() {
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void gotToReefLevel2(){
-        targetPosition = 2;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void gotToReefLevel1() {
+        targetPosition = MotorSetPoint.ELEVATATOR_REEF_LEVEL_1;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void gotToReefLevel3(){
-        targetPosition = 3;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void gotToReefLevel2() {
+        targetPosition = MotorSetPoint.ELEVATATOR_REEF_LEVEL_2;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void gotToReefLevel4(){
-        targetPosition = 4;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void gotToReefLevel3() {
+        targetPosition = MotorSetPoint.ELEVATATOR_REEF_LEVEL_3;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void goToAlgae1(){
-        targetPosition = 5;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
-    }
-    
-    public void goToBarge(){
-        targetPosition = 6;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void gotToReefLevel4() {
+        targetPosition = MotorSetPoint.ELEVATATOR_REEF_LEVEL_4;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void gotToHPStation(){
-        targetPosition = 7;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void goToAlgae1() {
+        targetPosition = MotorSetPoint.ELEVATATOR_ALGEA_LEVEL_1;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void goToAlgae2(){
-        targetPosition = 8;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
+    public void goToBarge() {
+        targetPosition = MotorSetPoint.ELEVATATOR_BARGE;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
 
-    public void goToAlgae3(){
-        targetPosition = 9;
-        controller.setReference(targetPosition, ControlType.kMAXMotionVelocityControl);
-    
+    public void gotToHPStation() {
+        targetPosition = MotorSetPoint.ELEVATATOR_HP;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
     }
-    public boolean isAtPosition(){
-        return Math.abs(encoder.getPosition() - targetPosition) <= 2; //MARGIN OF ERROR
-      }
+
+    public void goToAlgae2() {
+        targetPosition = MotorSetPoint.ELEVATATOR_REEF_LEVEL_2;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
+    }
+
+    public void goToAlgae3() {
+        targetPosition = MotorSetPoint.ELEVATATOR_ALGEA_LEVEL_3;
+        controller.setReference(targetPosition, ControlType.kMAXMotionPositionControl);
+
+    }
+
+    public boolean isAtPosition() {
+        return Math.abs(encoder.getPosition() - targetPosition) <= 2; // MARGIN OF ERROR
+    }
 }
-
