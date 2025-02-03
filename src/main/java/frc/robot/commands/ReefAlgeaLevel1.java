97cb3b0f9a4ffa44877aa94f13bfeff15e86b1c7
SeshMelchor34 <eltechau20@gmail.com>
2025-02-03 18:22:10 -0500

    Create ReefAlgeaLevel1.java

---
+++ src/main/java/frc/robot/commands/ReefAlgeaLevel1.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ReefAlgeaLevel1 extends Command {
+  public ReefAlgeaLevel1() {
+ 
+  }
+}
