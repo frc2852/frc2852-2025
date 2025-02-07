9933332777cb0743fe56171357a666b477cb1ca6
SeshMelchor34 <eltechau20@gmail.com>
2025-02-07 10:21:14 -0500

    Human Player station and berge score commands

---
+++ src/main/java/frc/robot/commands/HSPickUp.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class HSPickUp extends SequentialCommandGroup {
+  public ReefScoreLevel1(Elevator elevator, Wrist wrist, Intake intake) {
+    addCommands(
+        new ParallelCommandGroup(
+            new InstantCommand(() -> elevator.gotToHPStation(), elevator),
+            new InstantCommand(() -> wrist.gotToHPStation(), wrist)),
+        new WaitUntilCommand(() -> elevator.isAtPosition() && wrist.isAtPosition()),
+        new InstantCommand(() -> intake.runIntake()),
+        new WaitCommand(2),
+        new InstantCommand(() -> intake.stopIntake()),
+        new ParallelCommandGroup(
+            new InstantCommand(() -> elevator.goToBottom(), elevator),
+            new InstantCommand(() -> wrist.goToBottom(), wrist)));
+  }
+}
