c41a358d1f70b41c9da8bdd442effe6fb0d49f95
DanDan-Dan <148914957+DanDan-Dan@users.noreply.github.com>
2025-02-15 11:48:12 -0500

    led
    -set and get logic
    -change to static
    -has pink and green for coral and algea

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -7,12 +7,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Colors;
+import frc.robot.Constants.DIO;
 
 public class LED extends SubsystemBase {
   private final PWM blinkin;
 
   public LED() {
-      blinkin = new PWM(0);
+      blinkin = new PWM(DIO.BLINKIN);
   }
   //blinkin.setSpeed(pattern.getValue())
   public void setColor(int color){
@@ -24,17 +25,18 @@ VIOLET
 TWINKLES_PARTY
 */
   public void setPink(){
-    setColor(Colors.HOT_PINK);
+    setColor(Colors.HOT_PINK); // coral in
   }
   public void setGreen(){
-    setColor(Colors.DARK_GREEN);
+    setColor(Colors.DARK_GREEN); // algea in
   }
   public void setViolet(){
-    setColor(Colors.VIOLET);
+    setColor(Colors.VIOLET); // coral rollers running
   }
   public void setParty(){
-    setColor(Colors.TWINKLES_PARTY);
+    setColor(Colors.TWINKLES_PARTY);// algea rollers runng
   }
-
-  
+  public void turnoff(){
+    setColor(0);
+  }
 }
