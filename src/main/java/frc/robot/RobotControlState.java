f52657e2d359c455160edff19fb6462a97baa3ed
Seshat Melchor <155834265+SeshMelchor34@users.noreply.github.com>
2025-02-21 15:35:54 -0500

    Calculation of area method added

--- src/main/java/frc/robot/RobotControlState.java
+++ src/main/java/frc/robot/RobotControlState.java
@@ -4,46 +4,61 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.Constants.ScoringLevel;
 import frc.robot.Constants.Side;
 
 /** Add your docs here. */
 public class RobotControlState {
 
+  private static double area;
+
   private static Side side;
-    private static ScoringLevel scoringLevel;
+  private static ScoringLevel scoringLevel;
 
-      private static boolean hasAlgea= false;
-      private static boolean hasCoral=false;
+  private static boolean hasAlgea = false;
+  private static boolean hasCoral = false;
 
-      public static boolean hasAlgea(){
-        return hasAlgea;
-      }
+  public static boolean hasAlgea() {
+    return hasAlgea;
+  }
 
-      public static void setHasAlgea(boolean value){
-        hasAlgea= value; 
-      }
+  public static void setHasAlgea(boolean value) {
+    hasAlgea = value;
+  }
 
-      public static boolean hasCoral(){
-        return hasCoral;
-      }
-      public static void setHasCoral(boolean value){
-        hasCoral= value; 
-      }
-    
-      public static Side getSide() {
-        return side;
-      }
-    
-      public static ScoringLevel getScoringLevel() {
-        return scoringLevel;
-      }
-    
-      public static void setSide(Side newSide) {
-        side = newSide;
-    }
-  
-    public static void setScoringLevel(ScoringLevel newScoringLevel) {
-      scoringLevel = newScoringLevel;
-    }
+  public static boolean hasCoral() {
+    return hasCoral;
+  }
+
+  public static void setHasCoral(boolean value) {
+    hasCoral = value;
+  }
+
+  public static Side getSide() {
+    return side;
+  }
+
+  public static ScoringLevel getScoringLevel() {
+    return scoringLevel;
+  }
+
+  public static void setSide(Side newSide) {
+    side = newSide;
+  }
+
+  public static void setScoringLevel(ScoringLevel newScoringLevel) {
+    scoringLevel = newScoringLevel;
+  }
+
+  public static void areaCalculation(Translation2d p1) {
+    double x1 = p1.getX();
+    double x2 = p1.getX();
+    double x3 = p1.getX();
+    double y1 = p1.getY();
+    double y2 = p1.getY();
+    double y3 = p1.getY();
+
+    double area = Math.abs(x1 * (y1 - y2) + x2 * (y3 - y2) + x3 * (y1 - y3)) / 2;
+  }
 }
